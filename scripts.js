f5cdfb190641ddc3787b51a78fe6ce53875e989e
Sharon Chen <sharonch3n@gmail.com>
2018-01-14 20:31:20 -0600

    try

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
 // Your scripts go here.
 
 console.log('hello world!');
+
+$("#backtotop").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#thisistop").offset().top
+    }, 2000);
+});
