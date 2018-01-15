4ceec33e4c4eb08a6a0eaa11e264597bc2b279d7
Sharon Chen <sharonch3n@gmail.com>
2018-01-14 20:47:29 -0600

    try it

--- scripts.js
+++ scripts.js
@@ -6,23 +6,10 @@ console.log('hello world!');
 //     $('html, body').animate({scrollTop: $("#thisistop").offset().top}, 4000);
 // });
 
-// handle links with @href started with '#' only
-$(document).on('click', 'a[href^="#"]', function(e) {
-    // target element id
-    var id = $(this).attr('href');
-    
-    // target element
-    var $id = $(id);
-    if ($id.length === 0) {
-        return;
-    }
-    
-    // prevent standard hash navigation (avoid blinking in IE)
-    e.preventDefault();
-    
-    // top position relative to the document
-    var pos = $id.offset().top;
-    
-    // animated top scrolling
-    $('body, html').animate({scrollTop: pos});
+$("#backtotop").click(function() {
+    $('html,body').animate({scrollTop: 20}, 2000);
 });
+
+
+
+
