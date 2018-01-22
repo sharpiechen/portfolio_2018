808185534c3e26bd6c7a8696e29b62d9aabecb93
Sharon Chen <sharonch3n@gmail.com>
2018-01-21 20:18:38 -0600

    don’t o thedon’t

--- scripts.js
+++ scripts.js
@@ -74,6 +74,7 @@ $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
+  .not('[href="#img"]')
   .click(function(event) {
     // On-page links
     if (
